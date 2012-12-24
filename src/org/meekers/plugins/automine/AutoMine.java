d3d3407f18a1cc676dacb2f1ee7d2a0734dae3aa
Jared Meeker <jared@meekers.org>
2012-12-24 13:30:45 -0700

    Added optional 4th argument to /amine that will build instead of mine

--- src/org/meekers/plugins/automine/AutoMine.java
+++ src/org/meekers/plugins/automine/AutoMine.java
@@ -8,6 +8,8 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -24,12 +26,19 @@ public class AutoMine extends JavaPlugin {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = Bukkit.getPlayer(sender.getName());
+        Inventory playerinv = player.getInventory();
         Location ploc = player.getLocation().clone();
         World pworld = player.getWorld();
+        Material material = null;
 
         if (cmd.getName().equalsIgnoreCase("amine")) {
-            if (args.length > 3 || args.length < 3) {
+            if (args.length > 4 || args.length < 3) {
                 return false;
+            }
+
+            if (args.length == 4) {
+                material = Material.valueOf(args[3].toUpperCase());
+                player.sendMessage("building with " + material.name());
             }
 
             // Force length and width to be positive, height can be negative or positive
@@ -37,26 +46,20 @@ public class AutoMine extends JavaPlugin {
             int width = Math.abs(Integer.parseInt(args[1]));
             int height = Integer.parseInt(args[2]);
 
-            int totalBlocks = Math.abs(length) * Math.abs(width) * Math.abs(height);
-            int neededExp = totalBlocks / this.getConfig().getInt("blocksper1xp");
-
-            if (neededExp == 0) {
-                neededExp = 1;
+            // height of -1 is the level we're on... give one more so it does what we expect
+            if (height < 0) {
+                height--;
             }
 
+            int totalBlocks = Math.abs(length * width * height);
             int playerExp = player.getLevel();
+            int blocksper1xp = this.getConfig().getInt("blocksper1xp");
+            int neededExp = 1; // Charge at least 1 xp
+            int MaxneededExp = totalBlocks / blocksper1xp;
 
-            if (playerExp < neededExp) {
-                player.sendMessage("Insufficient experience, " + neededExp + " necessary.");
-                return true;
-            } else {
-                player.setLevel(player.getLevel()-neededExp);
-                player.sendMessage("You spent "+neededExp+ " experience.");
+            if (MaxneededExp == 0) {
+                MaxneededExp = 1;
             }
-//            player.sendMessage("You need " + neededExp + " experience to mine " + totalBlocks + " blocks.");
-            //player.sendMessage("Your location is "+ploc.toString());
-            //player.sendMessage("You are facing " + ploc.getDirection().toString() );
-            // Get the blocks and change them to air
 
             // X is East (positive) to West (negative)
             // Z is North (negative) to South (positive)
@@ -79,66 +82,96 @@ public class AutoMine extends JavaPlugin {
                 facingX = true;
                 if (v_x > 0) {
                     baseLoc.setX(baseLoc.getX() + 1);
-//                    baseLoc.setZ(baseLoc.getZ() - (int)Math.ceil(width/2.0));
                     facingXpos = true;  //East
                 } else {
                     baseLoc.setX(baseLoc.getX() - 1);
-//                    baseLoc.setZ(baseLoc.getZ() + (int)Math.ceil(width/2.0));
                 }
             } else {
                 // length is Z, width is X
                 if (v_z > 0) {
                     baseLoc.setZ(baseLoc.getZ() + 1);
-//                    baseLoc.setX(baseLoc.getX() + (int)Math.ceil(width/2.0));
                     facingZpos = true; // South
                 } else {
                     baseLoc.setZ(baseLoc.getZ() - 1);
-//                    baseLoc.setX(baseLoc.getX() - (int)Math.ceil(width/2.0));
                 }
             }
 
-
-            // works for horiz
-            for (int l = 0; l < length; l++) {
-                if (facingX) {
-                    if (facingXpos) {
-                        blockLoc.setX(baseLoc.getX() + l);
-                    } else {
-                        blockLoc.setX(baseLoc.getX() - l);
-                    }
-                } else {
-                    if (facingZpos) {
-                        blockLoc.setZ(baseLoc.getZ() + l);
-                    } else {
-                        blockLoc.setZ(baseLoc.getZ() - l);
-                    }
-                }
-                for (int h = 0; h < Math.abs(height); h++) {
-                    if (height > 0) {
-                        blockLoc.setY(baseLoc.getY() + h);
-                    } else {
-                        blockLoc.setY(baseLoc.getY() - h);
-                    }
-                    for (int w = 0; w < width; w++) {
-                        if (facingX) {
-                            if (facingXpos) {
-                                blockLoc.setZ(baseLoc.getZ() + w);
-                            } else {
-                                blockLoc.setZ(baseLoc.getZ() - w);
-                            }
+            // Only charge for non-AIR blocks
+            int blockCount = 0;
+            boolean expSpent = false;
+            while (expSpent != true) {
+                building:
+                for (int l = 0; l < length; l++) {
+                    if (facingX) {
+                        if (facingXpos) {
+                            blockLoc.setX(baseLoc.getX() + l);
                         } else {
-                            if (facingZpos) {
-                                blockLoc.setX(baseLoc.getX() - w);
+                            blockLoc.setX(baseLoc.getX() - l);
+                        }
+                    } else {
+                        if (facingZpos) {
+                            blockLoc.setZ(baseLoc.getZ() + l);
+                        } else {
+                            blockLoc.setZ(baseLoc.getZ() - l);
+                        }
+                    }
+                    for (int h = 0; h < Math.abs(height); h++) {
+                        if (height > 0) {
+                            blockLoc.setY(baseLoc.getY() + h);
+                        } else {
+                            blockLoc.setY(baseLoc.getY() - h);
+                        }
+                        for (int w = 0; w < width; w++) {
+                            if (facingX) {
+                                if (facingXpos) {
+                                    blockLoc.setZ(baseLoc.getZ() + w);
+                                } else {
+                                    blockLoc.setZ(baseLoc.getZ() - w);
+                                }
                             } else {
-                                blockLoc.setX(baseLoc.getX() + w);
+                                if (facingZpos) {
+                                    blockLoc.setX(baseLoc.getX() - w);
+                                } else {
+                                    blockLoc.setX(baseLoc.getX() + w);
+                                }
+                            }
+                            Block baseBlock = pworld.getBlockAt(blockLoc);
+                            if (baseBlock.getType() != Material.BEDROCK && (baseBlock.getType() != Material.AIR || args.length == 4)) {
+                                if (playerExp >= neededExp) {
+                                    blockCount++;
+                                    if (blockCount % blocksper1xp == 0) {
+                                        neededExp++;
+                                    }
+                                    if (args.length == 4) {
+                                        if (playerinv.contains(material)) {
+                                            playerinv.removeItem(new ItemStack[]{new ItemStack(material.getId(), 1)});
+                                            baseBlock.setType(material);
+                                        } else {
+                                            player.sendMessage("You ran out of " + material.name() + " building block #" + blockCount);
+                                            blockCount--;
+                                            break building;
+                                        }
+                                    } else {
+                                        if (this.getConfig().getBoolean("dropsenabled")) {
+                                            baseBlock.breakNaturally();
+                                        } else {
+                                            baseBlock.setType(Material.AIR);
+                                        }
+                                    }
+                                } else {
+                                    expSpent = true;
+                                }
                             }
                         }
-                        Block baseBlock = pworld.getBlockAt(blockLoc);
-                        baseBlock.setType(Material.AIR);
                     }
                 }
+                expSpent = true;
             }
-
+            if (blockCount == 0) {
+                neededExp = 0;
+            }
+            player.setLevel(player.getLevel() - neededExp);
+            player.sendMessage("You spent " + neededExp + " experience on " + blockCount + " blocks.");
             return true;
         }
         return false;
